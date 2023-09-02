4ab20eda1b2eb3be10c331508ebc002051679a57
zebicv <vanjazebic.dev@gmail.com>
2023-09-02 10:59:07 +0200

    Page not found added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './welcome/login.component';
 import { SignupComponent } from './welcome/signup.component';
 import { HomeComponent } from './connecty/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
@@ -13,10 +14,16 @@ const appRoutes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'home', component: HomeComponent },
+  { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, SignupComponent],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    PageNotFoundComponent,
+  ],
   imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent],
