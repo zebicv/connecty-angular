ca1260a1b463bcdf7f8ac173af9f46175aa09f3d
zebicv <vanjazebic.dev@gmail.com>
2023-09-23 18:47:23 +0200

    Signup form styles edit

--- src/app/welcome/login.component.ts
+++ src/app/welcome/login.component.ts
@@ -7,7 +7,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'],
+  styleUrls: ['./login.component.css', './shared.css'],
 })
 export class LoginComponent {
   constructor(private router: Router) {}
